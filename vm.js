ab9940d74faeccbf8cb05f3b973efd16a48fa6f8
Tastyfruitloops <93471183+Tastyfruitloops@users.noreply.github.com>
2021-11-03 10:40:06 +0500

    Add files via upload

--- vm.js
+++ vm.js
@@ -3,99 +3,86 @@
   1-fib-> n
   2-nod-> a,b  
 */
-//Первые 10 элементов под циклы, флаги, тмп и т.д
-//0-2 i,j,k
-//3-4 flags
-//5-9 tmps
-//10-19 под ввод
+
 
 let fs = require('fs');
+
 let readFileSync=require('readline-sync');
 const { exit } = require('process');
-let text=fs.readFileSync(process.argv[2],"utf-8").toString().split('\r\n');
-console.log(text);
-let memory=[];
-for (let j=0;j<30;j++) {memory.push(0);}
-let com=0;
+let memory=fs.readFileSync(process.argv[2],"utf-8").toLowerCase().split(/\s+/);
+//console.log(memory);
 let i=0;
-
-if (process.argv[2]=="fib.txt")
+let tmp=0;
+while(memory[i]!=="bye")
 {
-    memory[20]=0;
-    memory[21]=1;
-    while(com<text.length)
-    {
-        switch (text[com])
+    switch (memory[i])
         {
+            case 'set':
+                memory[memory[i+1]]=parseInt(memory[i+2]);
+                i+=3;
+                break;
             case 'input':
                 let input=readFileSync.question('>');
-                memory[i+10]=parseInt(input);
-                i++;
-                break;
-            case 'repeat':
-                memory[0]=0; //цикл начинается с 1
-                memory[3]=1; //флаг на цикл
-                break;
-            case '{':
-                memory[6]=com; //тмп забивается под номер строки
-                break;
-            case '}':
-                memory[0]++;
-                memory[4]=(memory[0]<=memory[10]) ? 1 : 0; //флаг на конец цикла
-                com=(memory[4]==1) ? memory[6] : com;
+                memory[memory[i+1]]=parseInt(input);
+                i+=2;
                 break;
             case 'sum':
-                memory[22+memory[0]]=memory[20+memory[0]]+memory[21+memory[0]];
+                memory[memory[i+3]]=memory[memory[i+1]]+memory[memory[i+2]];
+                i+=4;
                 break;
-            case 'output':
-                console.log(memory[20+memory[10]]);
+            case 'raz':
+                memory[memory[i+3]]=memory[memory[i+1]]-memory[memory[i+2]];
+                i+=4;
                 break;
-        }
-        com++;
-        
-    }
-    
-    
-}
-if (process.argv[2]=='nod.txt')
-{
-    memory[0]=0;
-    while(com<text.length)
-    {
-        switch (text[com])
-        {
-            case 'input':
-                let input=readFileSync.question('>');
-                memory[i+10]=parseInt(input);
-                i++;
+            case 'mul':
+                memory[memory[i+3]]=memory[memory[i+1]]*memory[memory[i+2]];
+                i+=4;
                 break;
-            case 'while':
-                memory[4]=0; //флаг на условие, while кончается, когда 1
+            case 'del':
+                memory[memory[i+3]]=memory[memory[i+1]]/memory[memory[i+2]];
+                i+=4;
                 break;
-            case '{':
-                memory[6]=com;
+            case 'move':
+                i=parseInt(memory[i+1]);
                 break;
-            case '}':
-                memory[4]=(memory[10]==memory[11]) ? 0 : 1;
-                com=(memory[4]==1) ? memory[6] : com;
+            case 'equals':
+                memory[memory[i+3]]=1*(memory[memory[i+1]]==memory[memory[i+2]]);
+                i+=4;
                 break;
-            case 'compare':
-                memory[3]=(memory[10]>memory[11]) ? 1 : 0;
+            case 'is_less':
+                memory[memory[i+3]]=1*(memory[memory[i+1]]<memory[memory[i+2]]);
+                i+=4;
+                break; 
+            case 'is_bigger':
+                memory[memory[i+3]]=1*(memory[memory[i+1]]>memory[memory[i+2]]);
+                i+=4;
+                break;
+            case 'move_if':
+                i=(memory[memory[i+1]]) ? parseInt(memory[i+2]) : i+3;
+                break;
+            case 'move_if_not':
+                i=(!memory[memory[i+1]]) ? parseInt(memory[i+2]) : i+3;
+                break;
+            case 'increase':
+                memory[memory[i+1]]++;
+                i+=2;
                 break;
             case 'swap':
-                memory[5]=memory[10]; //tmp под одно из чисел
-                memory[10]=memory[10]-(memory[10]-memory[11])*memory[3];
-                memory[11]=memory[11]-(memory[11]-memory[5])*memory[3];
-                memory[5]=0;
+                memory[99]=memory[memory[i+1]];
+                memory[memory[i+1]]=memory[memory[i+2]];
+                memory[memory[i+2]]=memory[99];
+                memory[99]=0;
+                i+=3;
                 break;
-            case 'lowermax':
-                memory[4]=(memory[10]==memory[11]) ? 0 : 1;
-                memory[11]=memory[11]-memory[10]*memory[4];
             case 'output':
-                console.log(memory[10]);
+                console.log(memory[memory[i+1]]);
+                i+=2;
                 break;
-        }
-     com++;
-     console.log(memory);
-    }   
+        } 
+        //if (i==1) {break;}
+    //console.log(i);   
+    //console.log(memory[i]);
+    //console.log(memory[100],memory[101],memory[102]);
+    //tmp++;
+    //if (tmp>60) {break;}
 }
